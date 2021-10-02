f2c1cf245d718c903e1b5ed8b0d81acd95314cc3
SoNesky <gueronlj@gmail.com>
2021-10-02 01:40:41 -0400

    added country buttons for filter

--- app.js
+++ app.js
@@ -17,51 +17,76 @@ const allCategories = [
    'technology'
 ]
 
-let keyword = null
-let countryIndex = allCountries[0]
+let keyword = 'covid'
+let countryIndex = allCountries[22]
 let languageIndex = allLanguages[2]
 let categoryIndex = allCategories[5]
 
 const key = 'ad5efa85ecde97e26bac1ae906948e4a'
 let startDate = '2021-08-24'
 let keywords = `&keywords=${keyword}`
-let countries = `&countries=${countryIndex}`
-let languages = `&languages=${languageIndex}`
-let categories = `&cetegories=${categoryIndex}`
-let ajaxUrl = `http://api.mediastack.com/v1/news?access_key=${key}&date=${startDate},2021-12-31&limit=60`
 
-if (keywords) {////////changes query based on filters.
-   ajaxUrl = ajaxUrl + keywords
-}
-if (countries){
-   ajaxUrl = ajaxUrl + countries
-}
-if (languages){
-   ajaxUrl = ajaxUrl + languages
-}
-if (categories){
-   ajaxUrl = ajaxUrl + categories
-}
+let languages =`&languages=${languageIndex}`
+let categories = `&cetegories=${categoryIndex}`
+
+
 
 $(() => {
 
-   $.ajax(
-      {
-            url:ajaxUrl
+   $('.flag').on('click' , (event) => {
+      countryIndex = ((event.target).id);
+      console.log(countryIndex);
+      let countries = `&countries=${countryIndex}`
+      let ajaxUrl = `http://api.mediastack.com/v1/news?access_key=${key}&date=${startDate},2021-12-31&limit=60`
+
+      if (keywords) {////////changes query based on filters.
+         ajaxUrl = ajaxUrl + keywords
       }
-   ).then(
-      (data) => {
-         console.log(data);
-
-
-
-
-
-//----------------WARNINGGGG!!!!!!!!!!!-----DO NOT CROSS THIS LINE--------!!!!!!!!!!!!!!!!!!!!!!---------------------------
-      },
-      () => {
-         console.log("failed to get data");
+      if (countries){
+         ajaxUrl = ajaxUrl + countries
       }
-   )
+      if (languages){
+         ajaxUrl = ajaxUrl + languages
+      }
+      if (categories){
+         ajaxUrl = ajaxUrl + categories
+      }
+
+
+      $.ajax(
+         {
+               url:ajaxUrl
+         }
+      ).then(
+         (data) => {
+            let array = Object.values(data.data)
+            printData(array)
+   //----------------WARNINGGGG!!!!!!!!!!!-----DO NOT CROSS THIS LINE--------!!!!!!!!!!!!!!!!!!!!!!---------------------------
+         },
+         () => {
+            console.log("failed to get data");
+         }
+      )
+   })
 })
 //----------------WARNINGGGG!!!!!!!!!!!-----DO NOT CROSS THIS LINE--------!!!!!!!!!!!!!!!!!!!!!!---------------------------
+
+const printData = (array) => {
+   for (let i = 0; i < array.length; i++){
+      let url = array[i].url
+      let title = array[i].title
+      let description = array[i].description
+      let img = array[i].image
+      let date = array[i].published_at
+
+      const $article = $('<div>').addClass('article').appendTo($('#content'))
+      const $info = $('<div>').addClass('info').appendTo($article)
+      const $images = $('<div>').addClass('articleImage').appendTo($article)
+      const $title = $('<h3>').addClass('title').text(title).appendTo($info)
+      const $description = $('<p>').addClass('info').text(description).appendTo($info)
+      const $url = $('<a>').addClass('info').text(url).appendTo($info)
+      const $date = $('<p>').addClass('info').text(date).appendTo($info)
+      const $img = $('<img>').attr('src',img).appendTo($images)
+
+   }
+}
